4e971c2b2eedfbbca126d6fa97fd07a2d620b15a
AiSatan <aisatan.ne@gmail.com>
2014-07-13 17:05:29 +0400

    first

---
+++ TestDot42/MainActivity.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using Android.App;
+using Android.Os;
+using Android.Util;
+using Android.Widget;
+using Dot42;
+using Dot42.Manifest;
+using Java.Io;
+using Java.Net;
+using Java.Util;
+using Org.Apache.Http.Conn.Util;
+using TestDot42;
+using Android.Content;
+using Android.View;
+
+[assembly: Application("TestDot42")]
+
+// these replace similar entries in AndroidManifest.xml
+[assembly: UsesPermission(Android.Manifest.Permission.INTERNET)]
+[assembly: UsesPermission(Android.Manifest.Permission.ACCESS_NETWORK_STATE)]
+[assembly: UsesPermission(Android.Manifest.Permission.BATTERY_STATS)]
+
+namespace TestDot42
+{
+	[Activity]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			SetContentView(R.Layouts.MainLayout);
+
+			var btnStart = (Button)FindViewById(R.Ids.Start);
+			btnStart.Click += OnClickStart;
+
+			var btnStop = (Button)FindViewById(R.Ids.Stop);
+			btnStop.Click += OnClickStop;
+		}
+
+		private void OnClickStop(object sender, EventArgs e)
+		{
+			StopService(new Intent(this, typeof(HttpService)));
+		}
+
+		private void OnClickStart(object sender, EventArgs e)
+		{
+			StartService(new Intent(this, typeof(HttpService)));
+		}
+	}
+}
